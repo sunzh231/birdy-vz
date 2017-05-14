3e9c98568cab605f99af141fff8e3ef1c2ed2aef
sunzh <sunzh231@163.com>
2017-05-15 07:17:00 +0800

    code reviewing

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
 require './framework/bootstrap.inc.php';
 $host = $_SERVER['HTTP_HOST'];
 if (!empty($host)) {
-	$bindhost = pdo_fetch("SELECT * FROM ".tablename('site_multi')." WHERE bindhost = :bindhost", array(':bindhost' => $host));
-	if (!empty($bindhost)) {
-		header("Location: ". $_W['siteroot'] . 'app/index.php?i='.$bindhost['uniacid'].'&t='.$bindhost['id']);
-		exit;
-	}
+  $bindhost = pdo_fetch("SELECT * FROM ".tablename('site_multi')." WHERE bindhost = :bindhost", array(':bindhost' => $host));
+  if (!empty($bindhost)) {
+    header("Location: ". $_W['siteroot'] . 'app/index.php?i='.$bindhost['uniacid'].'&t='.$bindhost['id']);
+    exit;
+  }
 }
 $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
 if(strexists($agent, 'mobile') || !empty($_GPC['i'])) {
-	header('Location: ./app/index.php?' . $_SERVER['QUERY_STRING']);
+  header('Location: ./app/index.php?' . $_SERVER['QUERY_STRING']);
 } else {
-	header('Location: ./web/index.php?' . $_SERVER['QUERY_STRING']);
+  header('Location: ./web/index.php?' . $_SERVER['QUERY_STRING']);
 }
